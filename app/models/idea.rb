14fa5b7406848c6cd2271558b86580ec28cae904
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-09-05 15:07:49 -0600

    Add Image and IdeaImage models

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,8 @@
 class Idea < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :idea_images
+  has_many :images, through: :idea_images
   validates_presence_of :title
   validates_presence_of :body
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -8,5 +8,7 @@ describe Idea, type: :model do
 
   describe 'relationships' do
     it {should belong_to :user}
+    it {should have_many :idea_images}
+    it {should have_many(:images).through(:idea_images)}
   end
 end
